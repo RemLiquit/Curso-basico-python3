32a818c75cb878950c0fbf626b85cce85c9671fa
RemLiquit <ceixpp@gmail.com>
2020-09-15 17:50:00 -0400

    Cadena de Caracteres

---
+++ cadena_caracteres.py
@@ -0,0 +1,44 @@
+# >>> nombre = input('Cual es tu nombre:')
+# Cual es tu nombre:diego
+# >>> nombre.upper()
+# 'DIEGO'
+# >>> nombre.lowcase
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# AttributeError: 'str' object has no attribute 'lowcase'
+# >>> nombre.capitalize()
+# 'Diego'
+# >>> nombre
+# 'diego'
+# >>> nombre = nombre.capitalize()
+# >>> nombre
+# 'Diego'
+# >>> nombre = nombre.strip()
+# >>> nombre
+# 'Diego'
+# >>> nombre = nombre.lower()
+# >>> diego
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'diego' is not defined
+# >>> nombre
+# 'diego'
+# >>> nombre = nombre.replace('o','a')
+# >>> nombre
+# 'diega'
+# >>> nombre [0]
+# 'd'
+# >>> letra = nombre [2]
+# >>> letra
+# 'e'
+# >>> lent(nombre)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'lent' is not defined
+# >>> len(nombre)
+# 5
+# >>> len(letra)
+# 1
+# >>> len('Hola! este es el curso de python')
+# 32
+# >>>
